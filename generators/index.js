15f60479cf2eb4eda13abf32ef4939f861697bf3
Volker Buzek <volker.buzek@js-soft.com>
2023-10-21 22:40:02 +0200

    feat: sub-gen, cmd line options

--- generators/index.js
+++ generators/index.js
@@ -3,9 +3,38 @@ const chalk = require("chalk")
 const yosay = require("yosay")
 const { execSync } = require("child_process")
 
+const wdi5 = {
+  defaults: {
+    configPath: "./webapp/test/e2e/",
+    specs: "./webapp/test/e2e/",
+    baseUrl: "http://localhost:8080"
+  }
+}
+
+/**
+ * this generator is a minimal wrapper around `npm init wdi5` and forwards all options to it
+ * if running "embedded" (as part of a parent generator, e.g. generator-ui5-project), it will not prompt for any options
+ * it also support cms line args, e.g. `yo wdi5 --configPath ./foo --specs ./bar --baseUrl http://localhost:8080 --ts`
+ * that will be forwarded to `npm init wdi5`
+ */
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts)
+
+    // use this.options.<option> laterz
+    this.option("embedded", { type: Boolean }) //> if run as part of a parent generator, e.g. generator-ui5-project
+    this.option("ts", { type: Boolean }) //> whether we're in TS-land, will be fwd to npm init
+    this.option("configPath", { type: String })
+    this.option("specs", { type: String })
+    this.option("baseUrl", { type: String })
+  }
+
   prompting() {
-    // Have Yeoman greet the user.
+    // don't prompt if running as sub-gen
+    if (this.options.embedded) {
+      return
+    }
+
     this.log(yosay(`Welcome to the ${chalk.red("wdi5")} generator!`))
 
     const prompts = [
@@ -13,19 +42,19 @@ module.exports = class extends Generator {
         type: "input",
         name: "configPath",
         message: "path to the 'wdio.conf.(j|t)s' file (relative to project root)",
-        default: "./webapp/test/e2e/"
+        default: wdi5.defaults.configPath
       },
       {
         type: "input",
         name: "specs",
         message: "path to the test files (relative to project root)",
-        default: "./webapp/test/e2e/"
+        default: wdi5.defaults.specs
       },
       {
         type: "input",
         name: "baseUrl",
         message: "URL of the UI5 app under test",
-        default: "http://localhost:8080"
+        default: wdi5.defaults.baseUrl
       }
     ]
 
@@ -36,9 +65,18 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    execSync(
-      `npm init wdi5 -y -- --configPath ${this.props.configPath} --specs ${this.props.specs} --baseUrl ${this.props.baseUrl}`,
-      { stdio: "inherit" }
-    )
+    const configPath = this.props?.configPath || this.options?.configPath || wdi5.defaults.configPath
+    const specs = this.props?.specs || this.options?.specs || wdi5.defaults.specs
+    const baseUrl = this.props?.baseUrl || this.options?.baseUrl || wdi5.defaults.baseUrl
+
+    process.env.DEBUG && this.log(`Generating wdi5 project with configPath=${configPath}, specs=${specs}, baseUrl=${baseUrl}`)
+
+    const cmd = `npm init wdi5@latest -y -- --configPath ${configPath} --specs ${specs} --baseUrl ${baseUrl}`
+
+    if (this.options?.ts === true) {
+      cmd.concat(" --ts")
+    }
+
+    execSync(cmd, { stdio: "inherit" })
   }
 }
